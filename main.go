02e37b6b8209db0ae27024aa234e04bdd61af761
Kazuki Sawada <kazuki@6715.jp>
2019-11-21 17:55:33 +0900

    mv cli

--- main.go
+++ main.go
@@ -4,11 +4,53 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/kaz/sql-replay/cli"
+	"github.com/kaz/sql-replay/digest"
+	"github.com/kaz/sql-replay/duplicate"
+	"github.com/urfave/cli"
+)
+
+var (
+	Version = "dev"
 )
 
 func main() {
-	if err := cli.Start(); err != nil {
+	app := cli.NewApp()
+
+	app.Name = "sql-replay"
+	app.Usage = "benchmarking tool for db"
+	app.Version = Version
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "digest",
+			Action: digest.Action,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "slowlog",
+				},
+				cli.StringFlag{
+					Name: "pcap",
+				},
+				cli.StringFlag{
+					Name: "output",
+				},
+			},
+		},
+		{
+			Name:   "duplicate",
+			Action: duplicate.Action,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "yaml",
+				},
+				cli.StringFlag{
+					Name: "sql",
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(-1)
 	}
